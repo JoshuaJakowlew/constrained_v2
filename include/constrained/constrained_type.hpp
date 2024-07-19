25477f4a5e209094dc32b29194a1df00f1ff5ac8
Яковлев Георгий <darksmolensksky@gmail.com>
2024-07-19 16:40:39 +0300

    Add concepts for validators. Temporarily comment constrained_type's implementation Rename types

--- include/constrained/constrained_type.hpp
+++ include/constrained/constrained_type.hpp
@@ -6,6 +6,9 @@
 #include <iostream>
 
 #include <constrained/optimizer/constraints/optimize.hpp>
+#include <constrained/optimizer/fail_handlers/optimize.hpp>
+#include <constrained/optimizer/validators/optimize.hpp>
+
 #include <constrained/combinators/all.hpp>
 #include <constrained/value_pack/to.hpp>
 
@@ -14,6 +17,7 @@ namespace ct {
         return std::same_as<T, U>;
     };
 
+#pragma region concepts
     template <typename F, typename T>
     concept predicate = std::predicate<F, T const &>;
 
@@ -32,6 +36,12 @@ namespace ct {
         fail_handler<F, T> and
         std::is_nothrow_invocable_v<F, T &>;
 
+    template <typename F, typename T>
+    concept validator = std::predicate<F, T>;
+
+    template <typename F, typename T>
+    concept nothrow_validator = nothrow_predicate<F, T>;
+
     namespace detail {
         template <typename P, typename T>
         struct is_predicate_pack : std::false_type {};
@@ -40,12 +50,15 @@ namespace ct {
             requires (... && predicate<decltype(Ps), T>)
         struct is_predicate_pack<value_pack<Ps...>, T> : std::true_type {};
 
-        template <typename P, typename T>
+        template <typename H, typename T>
         struct is_fail_handler_pack : std::false_type {};
 
         template <auto... Hs, typename T>
             requires (... && fail_handler<decltype(Hs), T>)
         struct is_fail_handler_pack<value_pack<Hs...>, T> : std::true_type {};
+
+        template <typename P, typename T>
+        struct is_validator_pack : is_predicate_pack<P, T> {};
     } // namespace detail
 
     template <typename Pack, typename T>
@@ -54,141 +67,159 @@ namespace ct {
     template <typename Pack, typename T>
     concept fail_handler_pack = detail::is_fail_handler_pack<Pack, T>::value;
 
+    template <typename Pack, typename T>
+    concept validator_pack = detail::is_fail_handler_pack<Pack, T>::value;
+#pragma endregion
+
     struct nocheck {};
 
     // TODO: Optimize self predicates, fail handlers and validators
     template <
         typename T,
         typename ConstraintPack,
-        typename OnFailPack,
-        typename HasValuePack = ConstraintPack,
+        typename FailHandlerPack,
+        typename ValidatorPack = ConstraintPack,
         auto Eq = type_eq
     >
         requires
             predicate_pack<ConstraintPack, T> and
-            fail_handler_pack<OnFailPack, T> and
-            predicate_pack<HasValuePack, T>
+            fail_handler_pack<FailHandlerPack, T> and
+            predicate_pack<ValidatorPack, T>
 
     class constrained_type
     {
     public:
+        using optimized_constraints   = ConstraintPack::template then<constraints::pack::optimize<Eq>>;
+        using optimized_fail_handlers = FailHandlerPack::template then<fail_handlers::pack::optimize<Eq>>;
+        using optimized_validators    = ValidatorPack::template then<validators::pack::optimize<Eq>>;
 
-        using Constraints = ConstraintPack
-            ::template then<constraints::pack::optimize<Eq>>;
+// #pragma region default_constructor
+//         constexpr constrained_type() 
+//             noexcept(
+//                 std::is_nothrow_default_constructible_v<T> and
+//                 noexcept(check(Constraints{}))
+//             )
+//             requires std::is_default_constructible_v<T>
+//         {
+//             std::puts("default constructor");
+//             check(Constraints{});
+//         }
+// #pragma endregion
 
-        constexpr constrained_type() 
-            noexcept(
-                std::is_nothrow_default_constructible_v<T> and
-                noexcept(check(Constraints{}))
-            )
-            requires std::is_default_constructible_v<T>
-        {
-            std::puts("default constructor");
-            check(Constraints{});
-        }
+// #pragma region default_nocheck_constructor
+//         constexpr constrained_type(nocheck)
+//             noexcept(std::is_nothrow_default_constructible_v<T>)
+//             requires std::is_default_constructible_v<T>
+//         {
+//             std::puts("default nocheck constructor");
+//         };
+// #pragma endregion
 
-        constexpr constrained_type(nocheck)
-            noexcept(std::is_nothrow_default_constructible_v<T>)
-            requires std::is_default_constructible_v<T>
-        {
-            std::puts("default nocheck constructor");
-        };
+// #pragma region forwarding_coonstructor
+//         template <typename... Args>
+//         constexpr constrained_type(Args&&... args)
+//             noexcept(
+//                 std::is_nothrow_constructible_v<T, Args...> and
+//                 noexcept(check(Constraints{}))
+//             )
+//             requires std::is_constructible_v<T, Args...>
+//             : _value(std::forward<Args>(args)...)
+//         { 
+//             std::puts("emplace constructor");
+//             check(Constraints{});
+//         }
+// #pragma endregion
 
-        template <typename... Args>
-        constexpr constrained_type(Args&&... args)
-            noexcept(
-                std::is_nothrow_constructible_v<T, Args...> and
-                noexcept(check(Constraints{}))
-            )
-            requires std::is_constructible_v<T, Args...>
-            : _value(std::forward<Args>(args)...)
-        { 
-            std::puts("emplace constructor");
-            check(Constraints{});
-        }
+// #pragma region copy_constructor
+//         constexpr constrained_type(constrained_type const & other)
+//             : _value(other.value())
+//         {
+//             std::puts("copy constructor (no checks performed)");
+//         }
+// #pragma endregion 
 
-        constexpr constrained_type(constrained_type const & other)
-            : _value(other.value())
-        {
-            std::puts("copy constructor (no checks performed)");
-        }
+// #pragma region move_constructor
+//         constexpr constrained_type(constrained_type && other)
+//             noexcept(std::is_nothrow_move_constructible_v<T>)
+//             requires std::move_constructible<T>
+//             : _value(std::move(other).value())
+//         {
+//             std::puts("move constructor (no checks performed)");
+//         }
+// #pragma endregion 
 
-        constexpr constrained_type(constrained_type && other)
-            noexcept(std::is_nothrow_move_constructible_v<T>)
-            requires std::move_constructible<T>
-            : _value(std::move(other).value())
-        {
-            std::puts("move constructor (no checks performed)");
-        }
+// #pragma region optimizing_copy_constructor
+//         template <predicate_pack<T> RhsConstraints>
+//             requires (!std::same_as<ConstraintPack, RhsConstraints>)
+//         constexpr constrained_type(constrained_type<T, RhsConstraints, OnFailPack, HasValuePack> const & other)
+//             // TODO: Add noexcept rules
+//             : _value(other.value())
+//         {
+//             std::puts("create from other constructor");
 
-        template <predicate_pack<T> RhsConstraints>
-            requires (!std::same_as<ConstraintPack, RhsConstraints>)
-        constexpr constrained_type(constrained_type<T, RhsConstraints, OnFailPack, HasValuePack> const & other)
-            // TODO: Add noexcept rules
-            : _value(other.value())
-        {
-            std::puts("create from other constructor");
+//             // TODO: Move these check to separate method because it's the same in copy and move constructors
+//             using constraints = Constraints::template then<constraints::construct::optimize<Eq, RhsConstraints>>;
+//             if (valid()) check(constraints{});
+//         }
+// #pragma endregion
 
-            // TODO: Move these check to separate method because it's the same in copy and move constructors
-            using constraints = Constraints::template then<constraints::construct::optimize<Eq, RhsConstraints>>;
-            if (valid()) check(constraints{});
-        }
+// #pragma region optimizing_move_constructor
+//         template <predicate_pack<T> RhsConstraints>
+//             requires (!std::same_as<ConstraintPack, RhsConstraints>)
+//         constexpr constrained_type(constrained_type<T, RhsConstraints, OnFailPack, HasValuePack> && other)
+//             // TODO: Add noexcept rules
+//             : _value(std::move(other).value())
+//         {
+//             std::puts("create from other constructor");
+//             using constraints = Constraints::template then<constraints::construct::optimize<Eq, RhsConstraints>>;
+//             if (valid()) check(constraints{});
+//         }
+// #pragma endregion
 
-        template <predicate_pack<T> RhsConstraints>
-            requires (!std::same_as<ConstraintPack, RhsConstraints>)
-        constexpr constrained_type(constrained_type<T, RhsConstraints, OnFailPack, HasValuePack> && other)
-            // TODO: Add noexcept rules
-            : _value(std::move(other).value())
-        {
-            std::puts("create from other constructor");
-            using constraints = Constraints::template then<constraints::construct::optimize<Eq, RhsConstraints>>;
-            if (valid()) check(constraints{});
-        }
+        // constexpr bool valid() const
+        //     noexcept(noexcept(valid_impl(HasValuePack{})))
+        // {
+        //     return valid_impl(HasValuePack{});
+        // }
 
-        constexpr bool valid() const
-            noexcept(noexcept(valid_impl(HasValuePack{})))
-        {
-            return valid_impl(HasValuePack{});
-        }
+        // constexpr explicit operator bool () const
+        //     noexcept(valid())
+        // { return valid(); }
 
-        constexpr explicit operator bool () const
-            noexcept(valid())
-        { return valid(); }
-
-        constexpr auto value() const & noexcept -> T const &
-        { return _value; }
-        constexpr auto value() && noexcept -> T &&
-        { return std::move(_value); }
-        constexpr auto value() const && noexcept -> T const &&
-        { return std::move(_value); }
+        // constexpr auto value() const & noexcept -> T const &
+        // { return _value; }
+        // constexpr auto value() && noexcept -> T &&
+        // { return std::move(_value); }
+        // constexpr auto value() const && noexcept -> T const &&
+        // { return std::move(_value); }
     private:
         T _value{}; // TODO: Allow non-initializing deafult construction via flags like nocheck
 
-        template <auto... Validator>
-        constexpr bool valid_impl(value_pack<Validator...>) const
-            noexcept(noexcept((... && Validator(_value))))
-        {
-            return (... && Validator(_value));
-        }
+        // template <auto... Validator>
+        // constexpr bool valid_impl(value_pack<Validator...>) const
+        //     noexcept(noexcept((... && Validator(_value))))
+        // {
+        //     return (... && Validator(_value));
+        // }
 
-        template <auto... Predicate>
-        constexpr void check(value_pack<Predicate...>)
-            noexcept(
-                noexcept(!(... && Predicate(_value))) and
-                noexcept(fail(OnFailPack{}))
-            )
-        {
-            std::cout << "[";
-            if (!(... && Predicate(_value)))
-                fail(OnFailPack{});
-            std::cout << "]" << std::endl;
-        }
+        // template <auto... Predicate>
+        // constexpr void check(value_pack<Predicate...>)
+        //     noexcept(
+        //         noexcept(!(... && Predicate(_value))) and
+        //         noexcept(fail(OnFailPack{}))
+        //     )
+        // {
+        //     std::cout << "[";
+        //     if (!(... && Predicate(_value)))
+        //         fail(OnFailPack{});
+        //     std::cout << "]" << std::endl;
+        // }
 
-        template <auto... FailHandler>
-        constexpr void fail(value_pack<FailHandler...>)
-            noexcept (noexcept((... , FailHandler(_value))))
-        {
-            (... , FailHandler(_value));
-        }
+        // template <auto... FailHandler>
+        // constexpr void fail(value_pack<FailHandler...>)
+        //     noexcept (noexcept((... , FailHandler(_value))))
+        // {
+        //     (... , FailHandler(_value));
+        // }
     };
 } // namespace ct {
