dda910103a4a4634835ca41b06fc012fbdaaf380
Яковлев Георгий <darksmolensksky@gmail.com>
2025-02-24 00:51:12 +0300

    Review concat operation - review tests - remove concat_pack_of_packs

--- include/constrained/value_pack/concat.hpp
+++ include/constrained/value_pack/concat.hpp
@@ -3,7 +3,8 @@
 #include <constrained/value_pack/value_pack.hpp>
 
 namespace ct {
-    // Concat with values
+    // Concats pack with raw values
+    // value_pack<1>::then<concat<2>> -> value_pack<1, 2>
     template <auto... Ys>
     struct concat
     {
@@ -11,12 +12,14 @@ namespace ct {
         using type = value_pack<Xs..., Ys...>;
     };
 
-    // Concat with packs
+
+    // Concats pack with other packs
+    // value_pack<1>::then<concat<value_pack<2>>> -> value_pack<1, 2>
     template <typename... Packs>
     struct concat_pack;
 
-    template <typename Head, typename... Tail>
-        requires (is_value_pack<Head>::value && ... && is_value_pack<Tail>::value)
+    // multiple pack case
+    template <traits::value_pack Head, traits::value_pack... Tail>
     struct concat_pack<Head, Tail...>
     {
         template <auto... Xs>
@@ -25,6 +28,7 @@ namespace ct {
             ::template then<concat_pack<Tail...>>;
     };
 
+    // single pack case, base case
     template <auto... Ys>
     struct concat_pack<value_pack<Ys...>>
     {
@@ -32,28 +36,18 @@ namespace ct {
         using type = value_pack<Xs..., Ys...>;
     };
 
+    // no pack case, base case
     template <>
     struct concat_pack<>
     {
         template <auto... Xs>
         using type = value_pack<Xs...>;
     };
-
-    // Concat list of packs
-    template <class... Packs>
-    struct concat_pack_of_packs;
-
-    template <class Head, class... Tail>
-        requires (is_value_pack<Head>::value && ... && is_value_pack<Tail>::value)
-    struct concat_pack_of_packs<Head, Tail...>
-    {
-        using type = Head
-            ::template then<concat_pack<Tail...>>;
-    };
-
-    template <>
-    struct concat_pack_of_packs<>
-    {
-        using type = value_pack<>;
-    };
 } // namespace ct
+
+namespace ct::traits {
+    template <typename... Packs>
+    concept concat_packable = requires {
+        typename concat_pack<Packs...>::template type<>;
+    };
+}
--- tests/src/value_pack/concat.cpp
+++ tests/src/value_pack/concat.cpp
@@ -2,31 +2,42 @@
 
 #include <constrained/value_pack/concat.hpp>
 
-namespace ct::test {
-    using p1 = value_pack<0, 1, 2>;
-    using p2 = value_pack<3, 4, 5>;
-    using p3 = value_pack<0, 1, 2, 3, 4, 5>;
+namespace ct::test::concat_ {
+    consteval void test_concat() {
+        static_assert(std::same_as<empty::then<concat<>>, empty>); // [] ++ [] == []
+        
+        static_assert(std::same_as<empty::then<concat<1>>, value_pack<1>>); // [] ++ [y] == [y]
+        static_assert(std::same_as<value_pack<1>::then<concat<>>, value_pack<1>>); // [x] ++ [] == [x]
+        static_assert(std::same_as<value_pack<1>::then<concat<2>>, value_pack<1, 2>>); // [x] ++ [y] == [x:y]
 
-    static_assert(std::same_as<empty::then<concat<>>, empty>);     // 0 + 0 == 0
-    static_assert(std::same_as<p1::then<concat<>>, p1>);           // x + 0 == x
-    static_assert(std::same_as<empty::then<concat<0, 1, 2>>, p1>); // 0 + x == x
-    static_assert(std::same_as<p1::then<concat<3, 4, 5>>, p3>);    // x + y == x + y
+        static_assert(std::same_as<empty::then<concat<1, 2, 3>>, value_pack<1, 2, 3>>); // [] ++ [ys] == [ys]
+        static_assert(std::same_as<value_pack<1, 2, 3>::then<concat<>>, value_pack<1, 2, 3>>); // [xs] ++ [] == [xs]
+        static_assert(std::same_as<value_pack<1, 2, 3>::then<concat<4, 5, 6>>, value_pack<1, 2, 3, 4, 5, 6>>); // [xs] ++ [ys] == [xs:ys]
+    }
 
-    static_assert(std::same_as<empty::then<concat_pack<>>, empty>);      // 0 + 0 == 0
-    static_assert(std::same_as<p1::then<concat_pack<>>, p1>);            // x + 0 == 0
-    static_assert(std::same_as<empty::then<concat_pack<empty>>, empty>); // 0 + 0 == 0
-    static_assert(std::same_as<empty::then<concat_pack<p1>>, p1>);       // x + 0 == x
-    static_assert(std::same_as<p1::then<concat_pack<empty>>, p1>);       // 0 + x == x
-    static_assert(std::same_as<p1::then<concat_pack<p2>>, p3>);          // x + y == x + y
-    
-    static_assert(std::same_as<empty::then<concat_pack<empty, empty>>, empty>);                               // (0 + 0) + 0 == 0
-    static_assert(std::same_as<empty::then<concat_pack<empty, empty, empty>>, empty>);                        // ((0 + 0) + 0) + 0 == 0
-    static_assert(std::same_as<p1::then<concat_pack<p2, p1>>, value_pack<0, 1, 2, 3, 4, 5, 0, 1, 2>>);        // (x + y) + z == x + y + z
-    static_assert(std::same_as<p1::then<concat_pack<p2, empty, p1>>, value_pack<0, 1, 2, 3, 4, 5, 0, 1, 2>>); // ((x + y) + 0) + z == x + y + z
+    template <auto... Xs>
+    struct another_pack {};
 
-    static_assert(std::same_as<concat_pack_of_packs<>::type, empty>);                                           // concat([]) == 0
-    static_assert(std::same_as<concat_pack_of_packs<empty>::type, empty>);                                      // concat([0]) == 0
-    static_assert(std::same_as<concat_pack_of_packs<empty, empty>::type, empty>);                               // concat([0, 0]) == 0
-    static_assert(std::same_as<concat_pack_of_packs<p1, empty, p2, empty>::type, p3>);                          // concat([x, 0, y, 0]) == concat([x, y])
-    static_assert(std::same_as<concat_pack_of_packs<p1, p2, p1>::type, value_pack<0, 1, 2, 3, 4, 5, 0, 1, 2>>); // concat([x, y, x]) == // concat([x, y, x]) 
-} // namespace ct::test
+    consteval void test_concat_pack() {
+        static_assert(!traits::concat_packable<another_pack<1>>); // concats only value_packs
+        static_assert(traits::concat_packable<>); // ++ [] is ok
+        static_assert(traits::concat_packable<empty>); // ++ [[]] is ok
+        static_assert(traits::concat_packable<value_pack<1>>); // ++ [[1]] is ok
+        static_assert(traits::concat_packable<value_pack<1>, empty, value_pack<2>, empty>); // ++ [[1], [], [2], []] is ok
+
+        static_assert(std::same_as<empty::then<concat_pack<>>, empty>); // [] ++ [] = []
+        static_assert(std::same_as<empty::then<concat_pack<empty>>, empty>); // [] ++ [[]] = []
+        static_assert(std::same_as<empty::then<concat_pack<empty, empty>>, empty>); // [] ++ [[], []] = []
+
+        static_assert(std::same_as<empty::then<concat_pack<value_pack<1>>>, value_pack<1>>); // [] ++ [[1]] = [1]
+        static_assert(std::same_as<empty::then<concat_pack<value_pack<1>, value_pack<2, 3>>>, value_pack<1, 2, 3>>); // [] ++ [[1], [2, 3]] = [1, 2, 3]
+        
+        static_assert(std::same_as<value_pack<1>::then<concat_pack<empty>>, value_pack<1>>); // [1] ++ [[]] = [1]
+        static_assert(std::same_as<value_pack<1, 2, 3>::then<concat_pack<empty>>, value_pack<1, 2, 3>>); // [1, 2, 3] ++ [[]] = [1, 2, 3]
+
+        static_assert(std::same_as<value_pack<1, 2, 3>::then<concat_pack<value_pack<4, 5>>>, value_pack<1, 2, 3, 4, 5>>); // [1, 2, 3] ++ [[4, 5]] = [1, 2, 3, 4, 5]
+        static_assert(std::same_as<value_pack<1, 2>::then<concat_pack<value_pack<3, 4>, value_pack<5>>>, value_pack<1, 2, 3, 4, 5>>); // [1, 2] ++ [[3, 4], [5]] = [1, 2, 3, 4, 5]
+
+        static_assert(std::same_as<value_pack<1, 2>::then<concat_pack<empty, value_pack<3, 4>, empty, value_pack<5>, empty>>, value_pack<1, 2, 3, 4, 5>>); // [1, 2] ++ [[], [3, 4], [], [5], []] = [1, 2, 3, 4, 5]
+    }
+} // namespace ct::test::concat_
