468439c512e0a84a39240adacda5cc895e8473fa
Яковлев Георгий <darksmolensksky@gmail.com>
2025-02-24 00:15:13 +0300

    Review ct::to tests

--- tests/src/value_pack/to.cpp
+++ tests/src/value_pack/to.cpp
@@ -2,12 +2,12 @@
 
 #include <constrained/value_pack/to.hpp>
 
-namespace ct::test {
-    using p1 = value_pack<0, 1, 2>;
-
+namespace ct::test::to_ {
     template <auto... Xs>
     struct another_pack {};
 
-    static_assert(std::same_as<empty::then<to<another_pack>>, another_pack<>>);
-    static_assert(std::same_as<p1::then<to<another_pack>>, another_pack<0, 1, 2>>);
-} // namespace ct::test
+    consteval void test_to() {
+        static_assert(std::same_as<empty::then<to<another_pack>>, another_pack<>>);
+        static_assert(std::same_as<value_pack<0, 1, 2>::then<to<another_pack>>, another_pack<0, 1, 2>>);
+    }    
+} // namespace ct::test::to_
